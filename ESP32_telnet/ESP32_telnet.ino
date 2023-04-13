3697374b27796e69ada42d33d6ed09e7a8e6d5c7
internetrelative <45829953+internetrelative@users.noreply.github.com>
2023-04-14 00:41:22 +0800

    Added support for multiple clients
    More than 1 clients is now supported. Server is also able to broadcast to all client that is connected.

--- ESP32_telnet/ESP32_telnet.ino
+++ ESP32_telnet/ESP32_telnet.ino
@@ -1,24 +1,28 @@
 #include <WiFi.h>
 
-const char* ssid = "YOUR_SSID";
-const char* password = "YOUR_PASSWORD";
+#define MAX_CLIENTS 2
+
+const char *ssid = "YOUR_SSID";
+const char *password = "YOUR_PASSWORD";
 
 // set both client and server to end with New Line aka LF , enable local echo on client terminal
 
-WiFiServer server(23);  // Telnet port
+WiFiServer server(23); // Telnet port
 IPAddress clientIP;
 
-void initWiFi() {
+void initWiFi()
+{
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   unsigned long lastTime = millis();
   Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(2, LOW);
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print('.');
     delay(1000);
     // Restart the ESP32 after 10s  has passed until WiFi is connected
-    if (millis() - lastTime >= 10000) {
+    if (millis() - lastTime >= 10000)
+    {
       ESP.restart();
     }
   }
@@ -27,7 +31,8 @@ void initWiFi() {
   WiFi.persistent(true);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   initWiFi();
   Serial.println("\nConnected to WiFi\n\n");
@@ -37,10 +42,15 @@ void setup() {
 
 unsigned long previousMillis = 0;
 unsigned long interval = 30000;
-void loop() {
+
+WiFiClient client[MAX_CLIENTS];
+unsigned long int lastUpTime[MAX_CLIENTS] = {0};
+void loop()
+{
   unsigned long currentMillis = millis();
   // if WiFi is down, try reconnecting
-  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >= interval)) {
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >= interval))
+  {
     Serial.print(millis());
     Serial.println("Reconnecting to WiFi...");
     WiFi.disconnect();
@@ -48,77 +58,105 @@ void loop() {
     previousMillis = currentMillis;
   }
 
-    WiFiClient client = server.available();
+  else
+  {
+    uint8_t i;
+    // If there's client waiting to connect to server
+    if (server.hasClient())
+    {
+      for (i = 0; i < MAX_CLIENTS; i++)
+      {
+        // if there's available slot and client doesn't exist
+        if (!client[i] || !client[i].connected())
+        {
+          client[i] = server.available();
+          Serial.printf("New client: %d %s\n\r", i, client[i].remoteIP().toString().c_str());
+          client[i].write("Server -> Hello World\n\r"); // Greet user when they connect
+          break;
+        }
+      }
+      if (i >= MAX_CLIENTS)
+      {
+        // reject client if there's no available slot
+        Serial.println("Rejecting new client");
+        server.available().stop();
+      }
+    }
 
-    if (client) {
-      clientIP = client.remoteIP();
-      Serial.printf("New client connected from %s\n\r", clientIP.toString().c_str());
+    else
+    {
+      for (i = 0; i < MAX_CLIENTS; i++)
+      {
+        if (client[i].connected())
+        {
+          // if client is connected, update lastUpTime
+          lastUpTime[i] = millis();
 
-      client.write("Server -> Hello World\n\r"); // Greet user when they connect
+          ///////////////////////////////////////  RECEIVING  DATA /////////////////////////////////////////
+          bool firstLine = true;
+          bool skip_telnet_negotiation = true;
+          while (client[i].available()) // if there's data from client
+          {
+            char c = client[i].read();
 
-      unsigned long lastTime = millis(); 
-
-
-      while (client.connected()) {
-        ///////////////////////////////////////  RECEIVING  DATA /////////////////////////////////////////
-        bool firstLine = true;
-        while (client.available()) {
-          char c = client.read();
-          // wait for 500ms to pass before reading from input stream
-          if (millis() - lastTime >= 500) {
-            //remove ascii that aren't needed
-            if (isAscii(c) && (int(c) > 31 && int(c) < 127)) {
-              if (firstLine) {
-                Serial.print("Client -> ");
+            if (isAscii(c) && (int(c) > 31 && int(c) < 127))
+            {
+              if (firstLine)
+              {
+                Serial.printf("Client %d -> ", i);
                 firstLine = false;
               }
               Serial.write(c);
             }
-            if (c == '\n') {
+            if (c == '\n')
+            {
               firstLine = true;
               Serial.printf("\n\r");
             }
           }
+          ////////////////////////////////////////////////////////////////////////////////////////////////////
         }
-        ///////////////////////////////////////////////////////////////////////////////////
-
-        ///////////////////////////////// SENDING DATA ///////////////////////////////////
-        while (Serial.available()) {
-          String inputString = Serial.readStringUntil('\n');
-          const char* cstr = inputString.c_str();
-          if (strcmp(cstr, "restart") == 0) {
-            Serial.printf("Client disconnected from serial: %s\n\r", clientIP.toString().c_str());
-            client.stop();
-            ESP.restart();
+        else
+        {
+          // if client is disconnected, check if it's been disconnected for more than 1s before closing the connection
+          if (lastUpTime[i] != 0 && millis() - lastUpTime[i] >= 1000)
+          {
+            Serial.printf("Client %d disconnected\n\r", i);
+            client[i].stop();
+            lastUpTime[i] = 0;
           }
-
-          if (strcmp(cstr, "client_disconnect") == 0) {
-            Serial.printf("Client %s disconnected from serial\n\r", clientIP.toString().c_str());
-            client.stop();
-            return;
-          }
-
-          //log to serial
-          Serial.printf("Sending to client -> %s\n\r", cstr);
-
-          //send to client
-          client.write("Server -> ");
-          client.write(cstr);
-          client.write("\n\r");
         }
       }
-      /////////////////////////////////////////////////////////////////////////////////
-
-      client.stop();
-      Serial.printf("Client disconnected: %s\n\r", clientIP.toString().c_str());
     }
+  }
 
-  // Input from serial when no client is connected
-  while (Serial.available()) {
+  // incoming serial data
+  while (Serial.available())
+  {
     String inputString = Serial.readStringUntil('\n');
-    const char* cstr = inputString.c_str();
-    if (strcmp(cstr, "restart") == 0) {
+    const char *cstr = inputString.c_str();
+
+    if (strcmp(cstr, "restart") == 0)
+    {
       ESP.restart();
     }
+
+    ///////////////////////////////// SENDING DATA ///////////////////////////////////
+    // send to all clients that are connected
+    for (uint8_t i = 0; i < MAX_CLIENTS; i++)
+    {
+      if (client[i])
+      {
+        if (client[i].connected())
+        {
+          clientIP = client[i].remoteIP();
+          Serial.printf("Sending to client %d -> %s\n\r", i, cstr);
+          client[i].write("Server -> ");
+          client[i].write(cstr);
+          client[i].write("\n\r");
+        }
+      }
+    }
+    /////////////////////////////////////////////////////////////////////////////////////
   }
 }
